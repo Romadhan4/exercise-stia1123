d9504996f22f8f5083650960f1fb457d50fd6959
romadhan 268802 <80800775+Romadhan4@users.noreply.github.com>
2021-03-28 21:28:43 +0800

    Add files via upload

---
+++ Rabbit.java
@@ -0,0 +1,10 @@
+
+public class Rabbit {
+	String name = "Rabita";
+	int weight =2;
+	
+	void type () {
+		String animal = "Rabbit";
+		System.out.println (animal);
+	}
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Rabbit a = new Rabbit();
+		a.type();
+		
+		System.out.println ("Animal names :" + a.name);
+		System.out.println ("Animal weight :" + a.weight);
+		
+
+	}
+
+}
